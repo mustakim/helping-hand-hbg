2301f9dffe15dfc98a75e7d9c4e8ec1bee7958c4
mustakim <eribd.mustakim@gmail.com>
2020-04-23 18:41:11 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', function() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch('./index.html', function() {
+        browserSync.reload();
+    })
+
+    watch('./css/style.css', function() {
+        browserSync.reload();
+    })
+
+    watch('./js/app.js', function() {
+        browserSync.reload();
+    })
+});
