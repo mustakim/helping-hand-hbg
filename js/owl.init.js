92de35467c160bd748d28ee70981b7a10754e197
smrafi <smrafi4130@gmail.com>
2020-05-02 01:22:31 +0600

    asdsad

--- js/owl.init.js
+++ js/owl.init.js
@@ -1,42 +1,60 @@
 //Owl Carousel
 $('#customer-testi').owlCarousel({
-  loop:true,
+  loop: true,
   nav: false,
   dots: true,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
-  }
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+$('#customer-testii').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  autoplay: false,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
 });
 
 $('#owl-fade').owlCarousel({
-  loop:true,
+  loop: true,
   nav: false,
   dots: true,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
   animateOut: 'fadeOut',
   items: 1,
-})
-
+});
 
 $('#single-owl').owlCarousel({
-  loop:true,
+  loop: true,
   nav: false,
   dots: true,
-  autoplay:true,
-  autoplayTimeout:5000,
-  autoplayHoverPause:true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
   items: 1,
-})
+});
